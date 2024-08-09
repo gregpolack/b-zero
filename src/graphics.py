ce852311091ac2dd9853de033b89117697e16f87
GregPolack <grzelock@gmail.com>
2024-08-09 11:57:48 +0200

    setup base for player and floot objects

--- src/graphics.py
+++ src/graphics.py
@@ -1,17 +1,60 @@
 import pygame
 
 class Box:
-    def __init__(self, pos_x: float, pos_y: float, width: float, height: float, color: tuple):
+    def __init__(
+            self, 
+            pos_x: float, 
+            pos_y: float, 
+            width: float, 
+            height: float,
+            color: tuple
+    ):
+        self._rect = pygame.rect.Rect((pos_x, pos_y, width, height))
         self._color = color
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self._width = width
-        self._height = height
     
-    def draw(self, win: pygame.Surface):
-        pygame.draw.rect(win, 
-                         self._color, 
-                         pygame.Rect(self.pos_x, 
-                                     self.pos_y, 
-                                     self._width, 
-                                     self._height))
+    def draw(self, surface: pygame.Surface):
+        pygame.draw.rect(surface, self._color, self._rect)
+
+class PlayerBox(Box):
+    def __init__(
+            self, 
+            pos_x: float, 
+            pos_y: float, 
+            width: float, 
+            height: float, 
+            color: tuple
+    ):
+        super().__init__(
+            pos_x,
+            pos_y,
+            width,
+            height,
+            color
+        )  
+
+    def handle_keys(self):
+        key = pygame.key.get_pressed()
+        dist = 5
+        if key[pygame.K_LEFT]:
+            self._rect.move_ip(-dist, 0)
+        if key[pygame.K_RIGHT]:
+            self._rect.move_ip(dist, 0)
+            
+
+class FloorBox(Box):
+    def __init__(
+            self,
+            pos_x: float,
+            pos_y: float,
+            width: float,
+            height: float,
+            color: tuple
+    ):
+        super().__init__(
+            pos_x,
+            pos_y,
+            width,
+            height,
+            color
+        )
+
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import pygame
-from graphics import Box
+from graphics import PlayerBox, FloorBox
 
 def main():
 
@@ -10,18 +10,27 @@ def main():
     clock = pygame.time.Clock()
     is_running = True
     pygame.display.set_caption("B-Zero")
+
+    # Tuples for RGB values.
+    white = (255, 255, 255)
+    brown = (150, 75, 0)
     
-    # Initialize player object.
-    player = Box(30, 30, 60, 60, (255,255,255))
+    # Initialize game objects.
+    player = PlayerBox(50, 50, 20, 20, white)
+    floor = FloorBox(0, 1000, 60, 60, brown)
 
     # Game loop.
     while is_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_F11:
+                pygame.display.toggle_fullscreen()
         
         screen.fill(screen_color)
         player.draw(screen)
+        player.handle_keys()
+        floor.draw(screen)
 
         pygame.display.flip()
 
